ddf049b46fa4e5b2605ad0571820b6d02d7141a0
Subash-Gopal <91050840+Subash-Gopal@users.noreply.github.com>
2021-10-23 13:34:01 +0530

    test

--- stats-test.cpp
+++ stats-test.cpp
@@ -1,30 +1,16 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "catch.hpp"
-//#include "stats.h"
+#include "stats.h"
 
 #include <cmath>
 
-double Average(const std::vector<double>& input) {
-    
-    
-//     computedStats.average = 4.5;
-   return accumulate(input.begin(), input.end(), 0.0l) / input.size() ;
-    
-}
-
-TEST_CASE( "Average of a vector test cases" ) {
-   float epsilon = 0.001;
-   REQUIRE(std::abs(Average({1.5, 8.9, 3.2, 4.5}) - 4.525) < epsilon);
-}
-
 TEST_CASE("reports average, minimum and maximum") {
-    //auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
-    //float epsilon = 0.001;
-    //REQUIRE(std::abs(computedStats.average - 4.525) < epsilon);
-    REQUIRE(1 == 1);
-    //REQUIRE(std::abs(computedStats.max - 8.9) < epsilon);
-    //REQUIRE(std::abs(computedStats.min - 1.5) < epsilon);
+    auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
+    float epsilon = 0.001;
+    REQUIRE(std::abs(computedStats.average - 4.525) < epsilon);
+    REQUIRE(std::abs(computedStats.max - 8.9) < epsilon);
+    REQUIRE(std::abs(computedStats.min - 1.5) < epsilon);
 }
 
 // TEST_CASE("average is NaN for empty array") {
