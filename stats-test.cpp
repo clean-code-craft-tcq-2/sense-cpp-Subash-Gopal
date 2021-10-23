4aed60dcb2dbf65bc2bf594483ac2f61d3dbb6f8
Subash-Gopal <91050840+Subash-Gopal@users.noreply.github.com>
2021-10-23 11:42:12 +0530

    test

--- stats-test.cpp
+++ stats-test.cpp
@@ -5,6 +5,18 @@
 
 #include <cmath>
 
+static int Factorial( int number ) {
+   return number <= 1 ? number : Factorial( number - 1 ) * number;  // fail
+// return number <= 1 ? 1      : Factorial( number - 1 ) * number;  // pass
+}
+
+TEST_CASE( "Factorials of 1 and higher are computed (pass)", "[single-file]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
+
 TEST_CASE("reports average, minimum and maximum") {
     //auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
     //float epsilon = 0.001;
