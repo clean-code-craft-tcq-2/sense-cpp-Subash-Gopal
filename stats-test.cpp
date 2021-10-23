61c99dd1749729878863473679dc6d924399fe85
Subash-Gopal <91050840+Subash-Gopal@users.noreply.github.com>
2021-10-23 13:30:53 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -5,26 +5,12 @@
 
 #include <cmath>
 
-static int Factorial( int number ) {
-   //return number <= 1 ? number : Factorial( number - 1 ) * number;  // fail
- return number <= 1 ? 1      : Factorial( number - 1 ) * number;  // pass
-}
-
 double Average(const std::vector<double>& input) {
     
     
 //     computedStats.average = 4.5;
    return accumulate(input.begin(), input.end(), 0.0l) / input.size() ;
     
-}
-TEST_CASE( "Factorial of 0 is 1 (fail)", "[single-file]" ) {
-    REQUIRE( Factorial(0) == 1 );
-}
-TEST_CASE( "Factorials of 1 and higher are computed (pass)", "[single-file]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
 }
 
 TEST_CASE( "Average of a vector test cases" ) {
