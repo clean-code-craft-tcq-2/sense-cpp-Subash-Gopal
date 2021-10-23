4d3d557b3c01d87ddfca8f28fe536ba6f640590d
Subash-Gopal <91050840+Subash-Gopal@users.noreply.github.com>
2021-10-23 13:48:04 +0530

    test

--- stats.cpp
+++ stats.cpp
@@ -1,11 +1,10 @@
 #include "stats.h"
-namespace Statistics {
-    
-Stats ComputeStatistics(const std::vector<double>& input_vector ) {
-    Stats compute;
-    compute.max = *(std::max_element(input_vector.begin(), input_vector.end()));
-    compute.min = *(std::min_element(input_vector.begin(), input_vector.end()));
-    compute.average = (std::accumulate(input_vector.begin(), input_vector.end(),0.0L)) / input_vector.size();
-    
-    return compute;
+namespace Statistics {    
+    Stats ComputeStatistics(const std::vector<double>& input_vector ) {
+        Stats compute;
+        compute.max = *(std::max_element(input_vector.begin(), input_vector.end()));
+        compute.min = *(std::min_element(input_vector.begin(), input_vector.end()));
+        compute.average = (std::accumulate(input_vector.begin(), input_vector.end(),0.0L)) / input_vector.size();    
+        return compute;
+    }
 }
