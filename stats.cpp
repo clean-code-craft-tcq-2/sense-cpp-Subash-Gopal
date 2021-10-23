a20c45a70cd039992be49719bee383038d2bbe08
Subash-Gopal <91050840+Subash-Gopal@users.noreply.github.com>
2021-10-23 18:05:26 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -9,3 +9,28 @@ namespace Statistics {
         return compute;
     }
 }
+
+class StatsAlerter
+{
+    float maximumThreshold;
+    //int x;
+    //int y;
+    public:
+    statsAlerter(const float maxThreshold, const std::vector<IAlerter>& input_vector){
+        maximumThreshold = maxThreshold;
+       // int x =input_vector[0];
+        //int y =input_vector[1];
+        
+    }
+    std::vector<IAlerter>checkAndAlert(const std::vector<double>& input_vector){
+        IAlerter objAlert;
+        float max = *(std::max_element(input_vector.begin(), input_vector.end()));
+        if(max > maximumThreshold){
+            objAlert.emailAlert.emailSent = true;
+            objAlert.ledAlert.ledGlows = true;
+        }
+        else{
+            objAlert.emailAlert.emailSent = false;
+            objAlert.ledAlert.ledGlows = false;
+        }
+};
