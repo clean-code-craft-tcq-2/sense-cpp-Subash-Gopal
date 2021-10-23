0bfd8f17136bcd1ebe4b7686974f4b8d06a1ac38
Subash-Gopal <91050840+Subash-Gopal@users.noreply.github.com>
2021-10-23 18:05:36 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -20,7 +20,6 @@ TEST_CASE("average is NaN for empty array") {
     REQUIRE(std::isnan(computedStats.average));
 }
 
-
 // TEST_CASE("raises alerts when max is greater than threshold") {
 //     EmailAlert emailAlert;
 //     LEDAlert ledAlert;
