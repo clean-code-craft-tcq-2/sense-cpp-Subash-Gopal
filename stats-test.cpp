1336b16e3dde543b067991ec453b6635702def92
Subash-Gopal <91050840+Subash-Gopal@users.noreply.github.com>
2021-10-24 21:04:13 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -28,8 +28,8 @@ TEST_CASE("raises alerts when max is greater than threshold") {
     alertObj->ledAlert = &ledAlert;
     std::vector<IAlerter*> alerters = {alertObj};
     
-    const float maxThreshold = 10.2;
-    StatsAlerter statsAlerter(maxThreshold, alerters);
+    //const float maxThreshold = 10.2;
+    StatsAlerter statsAlerter(10.2, alerters);
     statsAlerter.checkAndAlert({99.8, 34.2, 4.5, 6.7});
 
     REQUIRE(emailAlert.emailSent);
